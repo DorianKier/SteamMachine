62513349d9cc5ee5ac7b00316eea8f3fcaeac12e
Dorian Kiewro <dorian.kiewro@gmail.com>
2022-07-03 15:02:15 +0200

    Add minimized file bat

--- SteamMachine.bat
+++ SteamMachine.bat
@@ -1,3 +1,8 @@
 @echo off
+if not "%minimized%"=="" goto :minimized
+set minimized=true
+start /min cmd /C "%~dpnx0"
+goto :EOF
+:minimized
 taskkill /F /IM explorer.exe
 "C:\Program Files (x86)\Steam\Steam.exe"
